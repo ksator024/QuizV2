37f49e2742bdaefba815207e246dd8d2799da9a9
hp <hp@Laptop-Christoph>
2025-06-10 10:56:16 +0200

    score bisschen bearbeiter

--- src/main/java/Quiz/Quiz/score.java
+++ src/main/java/Quiz/Quiz/score.java
@@ -1,6 +1,17 @@
 package Quiz.Quiz;
 
+import javafx.scene.control.TextField;
+
 public class score
 {
+	public TextField spielerNameField = new TextField();
+
+	public TextField inputName = new TextField();
+
+	public void Name()
+	{
+		inputName.setPromptText("Spielername");
+	}
+	
 	
 }
